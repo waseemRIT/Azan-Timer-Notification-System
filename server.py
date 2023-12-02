5202c2ec0d3c91e6bf874aec3e9f2f58fb3df0a5
waseemRIT <89963412+waseemRIT@users.noreply.github.com>
2023-12-02 15:55:49 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,100 +1,79 @@
 import socket
 import threading
 import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 
-# Authentication credentials
+# Constants for the server
+HOST = 'localhost'
+PORT = 9999
 USER = "admin"
 PASSWD = "password"
-URL = "https://www.khaleejtimes.com/prayer-time-uae/dubai"
 
+# Function to get Athan times using Aladhan API
+def get_athan_times():
+    params = {
+        "city": "Dubai",
+        "country": "United Arab Emirates",
+        "method": "2"  # Fiqh Hanafi
+    }
+    response = requests.get("http://api.aladhan.com/v1/timingsByCity", params=params)
+    if response.status_code == 200:
+        timings = response.json()['data']['timings']
+        return timings
+    else:
+        print("Error fetching prayer times:", response.status_code)
+        return {}
 
-# Scrape Athan times
-def scrape_athan_times():
-    response = requests.get(URL)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    prayer_times = {}
-    headers = ['Fajr', 'Sunrise', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
-    # The CSS selector needs to be updated according to the website's structure
-    time_elements = soup.select('.prayer-timing-wrap tbody tr:first-child td:not(:first-child)')
-    for header, time_element in zip(headers, time_elements):
-        prayer_times[header] = time_element.text.strip()
-    return prayer_times
+# This dictionary will store the Athan times
+athan_times = get_athan_times()
 
-
-# This will hold the scraped Athan times
-athan_times = {}
-
-
-# Update Athan times once a day
-def update_athan_times():
-    global athan_times
-    athan_times = scrape_athan_times()
-    # Schedule the next update for the next day
-    threading.Timer(86400, update_athan_times).start()
-
-
-# Start the update immediately when the server starts
-update_athan_times()
-
-
+# Client handler function
 def client_handler(client_socket):
     try:
-        # Receive the username and password from the client
-        username = client_socket.recv(1024).decode('utf-8')
-        password = client_socket.recv(1024).decode('utf-8')
+        # Authentication
+        username = client_socket.recv(1024).decode('utf-8').strip()
+        password = client_socket.recv(1024).decode('utf-8').strip()
 
-        # Check the credentials
         if username == USER and password == PASSWD:
             client_socket.send("Login successful".encode('utf-8'))
-            # Logged-in user interaction loop
             while True:
                 # Send choices to the client
-                client_socket.send(
-                    "\n[1] Send Fajr Time\n[2] Send Dhuhr Time\n[3] Send Asr Time\n[4] Send Maghrib Time\n[5] Send Isha Time\nType 'done' to exit\nChoice: ".encode(
-                        'utf-8'))
+                client_socket.sendall("Choose Athan time to send [Fajr, Dhuhr, Asr, Maghrib, Isha] or 'done' to exit: ".encode('utf-8'))
                 choice = client_socket.recv(1024).decode('utf-8').strip()
 
-                # Process the client's choice
-                if choice == "done":
-                    client_socket.send("Exiting".encode('utf-8'))
+                if choice.lower() == 'done':
+                    client_socket.sendall("Exiting".encode('utf-8'))
                     break
-                elif choice in ["1", "2", "3", "4", "5"]:
-                    # Send the corresponding Athan time
-                    times = list(athan_times.values())
-                    client_socket.send(
-                        f"{list(athan_times.keys())[int(choice) - 1]} time is at: {times[int(choice) - 1]}".encode(
-                            'utf-8'))
+                elif choice.capitalize() in athan_times:
+                    response = f"{choice.capitalize()} time is at: {athan_times[choice.capitalize()]}"
+                    client_socket.sendall(response.encode('utf-8'))
                 else:
-                    client_socket.send("Invalid choice. Please try again.".encode('utf-8'))
+                    client_socket.sendall("Invalid choice. Please try again.".encode('utf-8'))
         else:
-            client_socket.send("Login failed: invalid username or password".encode('utf-8'))
+            client_socket.sendall("Login failed: invalid username or password".encode('utf-8'))
+
     except Exception as e:
-        print(f"An exception occurred: {e}")
+        print(f"An exception occurred with client {client_socket.getpeername()}: {e}")
     finally:
         client_socket.close()
 
-
-def start_server(host='localhost', port=9999):
+# Function to start the server
+def start_server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((host, port))
-    server_socket.listen(5)
-    print(f"Server started on {host}:{port}. Waiting for connections...")
+    server_socket.bind((HOST, PORT))
+    server_socket.listen()
+    print(f"Server started on {HOST}:{PORT}. Waiting for connections...")
 
-    try:
-        while True:
-            client_sock, addr = server_socket.accept()
+    while True:
+        try:
+            client_socket, addr = server_socket.accept()
             print(f"Accepted connection from {addr}")
-            client_thread = threading.Thread(target=client_handler, args=(client_sock,))
-            client_thread.start()
-    except KeyboardInterrupt:
-        print("\nServer is shutting down.")
-    finally:
-        server_socket.close()
-        print("Server closed.")
+            threading.Thread(target=client_handler, args=(client_socket,), daemon=True).start()
+        except Exception as e:
+            print(f"An exception occurred: {e}")
 
+    server_socket.close()
 
+# Run the server
 if __name__ == "__main__":
     start_server()
